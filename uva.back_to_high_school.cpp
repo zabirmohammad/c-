dc6ffadee388a3bd18e9fb5843a1b966589dc216
Zabir Mohammad <61370054+zabirmohammad@users.noreply.github.com>
2025-10-04 19:41:52 +0600

    Add files via upload

---
+++ uva.back_to_high_school.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("output.txt","w",stdout);
+    int velocity,time;
+    while(cin>>velocity)
+    {
+        cin>>time;
+        cout<<2*velocity*time<<endl;
+    }
+    return 0;
+}
