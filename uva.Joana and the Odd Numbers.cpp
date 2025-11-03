0782741144e9e3d6af7ec3c058b4f5deb4758e3c
Zabir Mohammad <61370054+zabirmohammad@users.noreply.github.com>
2025-11-04 02:03:51 +0600

    J COMPLETE

---
+++ uva.Joana and the Odd Numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("output.txt","w",stdout);
+    long long n,t=-1,i,j,k=1;
+    while(cin>>n)
+    {
+        n=(n+1)/2;
+        cout<<3*(2*n*n-3)<<endl;
+    }
+}
